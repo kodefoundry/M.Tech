7eaebfadf854b88ed87e70fd4b08576cbf761a3e
Biju <36260059+kodefoundry@users.noreply.github.com>
2022-07-18 16:32:03 +0530

    Update split.py

--- SEM-II/Systems For Data Analytics/exams/assignment-I/split.py
+++ SEM-II/Systems For Data Analytics/exams/assignment-I/split.py
@@ -1,29 +1,32 @@
 import pandas as pd
 import numpy as np
+import os
+from filesplit.split import Split
+import glob
 
 # Define number of workers available, this can be passed a commandline also
 # For the scope of this assignment it's kept hardcoded
 no_workers = 4
 
-# Read geosales.csv as a pandas DataFrame
-df = pd.read_csv('geosales.csv')[['region','units_sold','total_profit']]
+size = os.path.getsize('geosales.csv')
+chunk_size = size/4
 
-# Use numpy to split the data frames in to smaller dataframes to be pickled
-df_chunks = np.array_split(df, no_workers)
+split = Split('geosales.csv', './')
+split.bysize(chunk_size,True,True)
+files = glob.glob('geosales_*.csv')
+files.sort()
 
-# Split the dataframes into smaller data frames equal to number of available workers and pickle
-for i,chunk in enumerate(df_chunks):
-    out_path = 'data_{}.pkl'.format(i+1)
-    chunk.to_pickle(out_path)
-
-# Test code to check if pickling was correct and no data loss
-# chunk_1_df = pd.read_pickle('assignment/data_1.pkl')
-# chunk_1_df.size
-
-
-
-
-
-
-
-
+last_file_id = len(files)
+last_df = pd.DataFrame(columns=['region','units_sold','total_profit'])
+for i,file in enumerate(files, start=1):
+    df = pd.read_csv(file)[['region','units_sold','total_profit']]
+    if i < 4:
+        # Pickle file 1 - 3 directly
+        df.to_pickle('data_{}.pkl'.format(i))
+        os.remove(file)
+    else:
+        last_df = pd.concat([last_df,df])
+        os.remove(file)
+        if i == last_file_id:
+            # 4 th chunk will be larger containing all data after 3rd chunk
+            last_df.to_pickle('data_4.pkl'.format(i))
